161359b398fa38298522fb66e3c8dd9838a492ea
Max Carmona <maxelias87@gmail.com>
2021-10-31 13:13:20 -0500

    creating android app

---
+++ android/BMICALC/app/src/main/java/co/edu/unab/BmiCalc/Calculator.java
@@ -0,0 +1,58 @@
+package co.edu.unab.BmiCalc;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.TextView;
+
+public class Calculator extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_calculator);
+
+        EditText weightInput = (EditText) findViewById(R.id.weightInput);
+        EditText heightInput = (EditText) findViewById(R.id.heightInput);
+        TextView bmiResult = (TextView) findViewById(R.id.bmiResult);
+        Button calcBtn = (Button) findViewById(R.id.calcBtn);
+        Button clearBtn = (Button) findViewById(R.id.clearBtn);
+        ImageButton returnBtn = (ImageButton) findViewById(R.id.returnBtn);
+
+        weightInput.requestFocus();
+
+        calcBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                float weight = Float.parseFloat(weightInput.getText().toString());
+                float height = Float.parseFloat(heightInput.getText().toString()) / 100;
+
+                float bmi = weight / (height * height);
+
+                bmiResult.setText(String.format("%, .2f", bmi));
+            }
+        });
+
+        clearBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                weightInput.setText("");
+                heightInput.setText("");
+                bmiResult.setText("");
+                weightInput.requestFocus();
+            }
+        });
+
+        returnBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+
+    }
+}
